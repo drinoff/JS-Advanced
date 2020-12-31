000d03e66ba99757862ce8436da3cf987d4bef66
Drinoff <drinoff@gmail.com>
2020-12-31 14:34:10 +0100

    up

--- DOM/07. Number-Convertor/solution.js
+++ DOM/07. Number-Convertor/solution.js
@@ -1,4 +1,38 @@
 function solve() {
 
-    //TODO...
+    let firstOptElement = document.createElement('option');
+    let secondOptElement = document.createElement('option');
+    firstOptElement.value = 'binary';
+    secondOptElement.value = 'hexadecimal';
+    firstOptElement.innerHTML = 'Binary';
+    secondOptElement.innerHTML = 'Hexadecimal';
+
+    let menuTOElement = document.getElementById('selectMenuTo');
+    menuTOElement.appendChild(firstOptElement);
+    menuTOElement.appendChild(secondOptElement);
+    
+    
+    let button = document.getElementsByTagName('button')[0];
+    
+    button.addEventListener('click', function(){
+        let numberInput = document.getElementById('input');
+        
+
+        let outputResult = document.getElementById('result');
+        if(menuTOElement.value == 'binary'){
+
+         
+         outputResult.value = Number(numberInput.value).toString(2);
+         
+
+        
+        }else if (menuTOElement.value == 'hexadecimal') {
+            
+        
+            outputResult.value = Number(numberInput.value).toString(16).toUpperCase();
+        }
+
+
+    });
+
 }
